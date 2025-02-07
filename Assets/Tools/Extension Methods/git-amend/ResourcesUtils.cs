373d0e42c25754533ab847e6317f2f3090ed4075
xidriel-sc <stormingisle@proton.me>
2025-02-08 01:39:50 +0300

    The whole collection of git-amend extensions

---
+++ Assets/Tools/Extension Methods/git-amend/ResourcesUtils.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+#if ENABLED_UNITY_URP
+using UnityEngine.Rendering;
+#endif
+
+namespace UnityUtils {
+    public static class ResourcesUtils {
+#if ENABLED_UNITY_URP
+        /// <summary>
+        /// Load volume profile from given path.
+        /// </summary>
+        /// <param name="path">Path from where volume profile should be loaded.</param>
+        public static void LoadVolumeProfile(this Volume volume, string path) {
+            var profile = Resources.Load<VolumeProfile>(path);
+            volume.profile = profile;
+        }
+#endif
+    }
+}
